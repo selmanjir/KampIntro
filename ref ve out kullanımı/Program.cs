0b5b08faaa06befbe551fd3ae3fa919fc2130626
Selman Jir CAN <selman_jir_85@hotmail.com>
2021-04-30 14:50:17 +0300

    .

--- ref ve out kullanımı/Program.cs
+++ ref ve out kullanımı/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 
-namespace ref_ve_out_kullanımı
-{
-    class Program
+namespace ref_out_kullanımı
+{   // ref ve out kullanımını sizlere daire alan,çevre hesaplama programı üzerinden göstermeye çalışacağım...
+    class Islem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double pi = 3.14;   //bildiğiniz üzere pi sayısı sabit ve yaklaşık olarak 3.14 de denk gelmektedir.
+            Console.ReadLine();
         }
     }
 }
